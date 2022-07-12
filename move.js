d534f6bfea349e293e3ed4de91bf76f6596b0624
Alexander-MD <Alexander-MD@outlook.com>
2022-07-11 22:06:39 -0400

    done

--- move.js
+++ move.js
@@ -1,12 +1,85 @@
 function move(element) {
-    element.style.position = 'fixed'
+  element.style.position = "fixed";
 
-    function moveToCoordinates(left, bottom) {
-        element.style.left = left + 'px'
-        element.style.bottom = bottom + 'px'
+  function moveToCoordinates(left, bottom) {
+    element.style.left = left + "px";
+    element.style.bottom = bottom + "px";
+  }
+
+  /*let direction = null;
+let x = 100;
+let y = 250;*/
+
+  function moveWithArrowKeys(left, bottom, callback) {
+    let direction = null;
+    let x = left;
+    let y = bottom;
+
+    element.style.left = x + "px";
+    element.style.bottom = y + "px";
+
+    function moveCharacter() {
+      if (direction === "west") {
+        x -= 1;
+      }
+      if (direction === "north") {
+        y += 1;
+      }
+      if (direction === "east") {
+        x += 1;
+      }
+      if (direction === "south") {
+        y -= 1;
+      }
+      element.style.left = x + "px";
+      element.style.bottom = y + "px";
     }
 
-    return {
-        to: moveToCoordinates
-    }
+    setInterval(moveCharacter, 1);
+
+    document.addEventListener("keydown", function (e) {
+      if (e.repeat) return;
+
+      if (e.key === "ArrowLeft") {
+        direction = "west";
+      }
+      if (e.key === "ArrowUp") {
+        direction = "north";
+      }
+      if (e.key === "ArrowRight") {
+        direction = "east";
+      }
+      if (e.key === "ArrowDown") {
+        direction = "south";
+      }
+      callback(direction);
+    });
+
+    document.addEventListener("keyup", function (e) {
+      direction = null;
+      callback(direction);
+    });
+  }
+
+  /*setInterval(function () {
+  if (direction === "west") {
+    x = x - 1;
+  }
+  if (direction === "east") {
+    x = x + 1;
+  }
+  if (direction === "north") {
+    y = y + 1;
+  }
+  if (direction === "south") {
+    y = y - 1;
+  }
+  character.style.left = x + "px";
+  character.style.bottom = y + "px";
+}, 1);*/
+
+  return {
+    to: moveToCoordinates,
+    withArrowKeys: moveWithArrowKeys,
+  };
 }
